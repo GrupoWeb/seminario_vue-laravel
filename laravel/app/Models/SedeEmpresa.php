7bdcfa3544c0c7a2bde3facd420ac4518b64f87e
juanjo <31669195+GrupoWeb@users.noreply.github.com>
2022-05-16 15:50:23 -0600

    mograciones de catalogos

---
+++ laravel/app/Models/SedeEmpresa.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SedeEmpresa extends Model
+{
+    use HasFactory;
+}
