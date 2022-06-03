e21ad0085be100a977a74b90143a323f45135a34
juanjo <31669195+GrupoWeb@users.noreply.github.com>
2022-06-03 17:03:56 -0600

    add

--- laravel/coreui/src/main.js
+++ laravel/coreui/src/main.js
@@ -15,7 +15,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'sweetalert2/dist/sweetalert2.min.css';
 
-Vue.prototype.$apiAdress = 'http://127.0.0.1:81'
+Vue.prototype.$apiAdress = 'http://localhost'
 Vue.config.performance = true
 Vue.use(CoreuiVue)
 Vue.use(BootstrapVue)
